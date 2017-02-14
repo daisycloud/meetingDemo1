3b17f3353e53de39fcc90048d3a75616c7fa7076
yyzeng <maplecloud21@hotmail.com>
2017-02-14 18:26:32 +0800

    图片，音频 图文 链接 视频 消息反应

---
+++ src/com/meeting/weixin/message/resp/VoiceMessage.java
@@ -0,0 +1,20 @@
+package com.meeting.weixin.message.resp;
+/**
+* 类名: VoiceMessage
+* 描述: 语音消息
+* 开发人员： yyzeng
+* 创建时间：  2017-2-14
+* 发布版本：V1.0  
+ */
+public class VoiceMessage extends BaseMessage {
+    // 语音
+    private Voice Voice;
+
+    public Voice getVoice() {
+        return Voice;
+    }
+
+    public void setVoice(Voice voice) {
+        Voice = voice;
+    }
+}
